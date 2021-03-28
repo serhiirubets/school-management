301a62564bc7470c30db258de2a691f31c5eb4a5
serhiirubets <serhii.rubets@gmail.com>
2021-03-28 12:13:26 +0300

    Init

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { LessonModule } from './lesson/lesson.module';
+
+
+@Module({
+  imports: [GraphQLModule.forRoot({ autoSchemaFile: true }), LessonModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
